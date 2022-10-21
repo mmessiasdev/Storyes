8338b4bb7aedcd3c93dd7b80ce58b52bf3000726
M. Messias <mmessiasdev@gmail.com>
2022-10-21 15:34:11 -0300

    V - 0.4 | Tela de Adicionar produtos pronta

--- front/reactcode/src/view/screens/addproduct/addproduct.jsx
+++ front/reactcode/src/view/screens/addproduct/addproduct.jsx
@@ -9,6 +9,8 @@ import logo from "../../assets/logo.png";
 import { BiLogIn } from "react-icons/bi";
 import axios from "axios";
 import { URLPOSTPRODUCT, URLREGISTER } from "../../../model/config";
+import authapi from "../../../services/authapi";
+import authcontext from "../../../services/authcontext";
 
 
 
@@ -16,54 +18,53 @@ import { URLPOSTPRODUCT, URLREGISTER } from "../../../model/config";
 
 const AddP = () => {
 
-    // const history = useNavigate();
-
-    // const [credentials, setCredentials] = useState({
-    //     name: "",
-    //     desc: "",
-    //     brand: "",
-    // });
-
-    // const handleChange = (e) => {
-    //     const { name, value } = e.target;
-    //     setCredentials({
-    //         ...credentials, [name]: value
-    //     });
-    // }
-
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     try {
-    //         const response = await fetch(
-    //             `${URLPOSTPRODUCT}`, {
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify({
-    //                 name: credentials.name,
-    //                 desc: credentials.desc,
-    //                 brand: credentials.brand,
-    //             }),
-    //             method: 'POST',
-    //         }
-    //         );
-    //         const data = await response.json();
-    //         console.log(data)
-    //     }
-    //     catch (error) {
-    //         console.error(error);
-
-    //     }
-    // }
+    const [product, setProduct] = useState([]);
+    const [newName, setNewName] = useState("");
+    const [newDesc, setNewDesc] = useState("");
+    const [newBrand, setNewBrand] = useState("");
 
 
-    const [sentPhoto, setSentPhoto] = useState('');
+    useEffect(() => {
+        update();
+    }, []);
 
-    function upload(e) {
-        e.preventDefault()
-        console.log("sentPhoto used!!")
+
+    function update() {
+        fetch('http://localhost:1337/api/products')
+            .then(res => res.json())
+            .then(product => {
+                setProduct(product.data);
+            })
     }
 
+
+    function addProduct(e) {
+        e.preventDefault();
+        let name = newName;
+        let desc = newDesc;
+        let brand = newBrand;
+        let body = {
+            data: {
+                name,
+                desc,
+                brand
+            }
+        };
+
+        fetch('http://localhost:1337/api/products', {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
+            .then(() => {
+                setNewDesc("");
+                setNewName("");
+                setNewBrand("");
+                update();
+            })
+    }
 
 
     return (
@@ -84,13 +85,18 @@ const AddP = () => {
                 <div className="addp">
                     <div className="left">
                         <h1>Adicione seu produto</h1>
-                        <form className="inputs" onSubmit={upload}>
+                        <form className="inputs" onSubmit={addProduct}>
+                            <div>
+                                <TextField value={newName} onChange={e => setNewName(e.currentTarget.value)} className="input" id="name" label="Nome do Produto" type="text" name="name" variant="filled" />
+                                <TextField value={newDesc} onChange={e => setNewDesc(e.currentTarget.value)} className="input" id="desc" label="Descrição do Produto" type="text" name="desc" variant="filled" />
+                                <TextField value={newBrand} onChange={e => setNewBrand(e.currentTarget.value)} className="input" id="brand" label="Marca do Porduto" type="text" name="brand" variant="filled" />
+                            </div>
 
-
+                            {/* 
                             <div className="sentphoto">
                                 <h4>Envie a imagem do Produto</h4>
                                 <input type="file" onChange={e => setSentPhoto(e.target.files[0])}></input>
-                            </div>
+                            </div> */}
 
                             <Button className="registerbutton" type="submit">
                                 <h2>Cadastrar Produto</h2>
