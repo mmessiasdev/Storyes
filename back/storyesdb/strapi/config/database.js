a2ebd0ce266f798eafb15e5059a4540d79249930
M. Messias <mmessiasdev@gmail.com>
2022-11-22 15:58:36 -0300

    Installed pg, aws-S3 upload provider and updated the config files

--- back/storyesdb/strapi/config/database.js
+++ back/storyesdb/strapi/config/database.js
@@ -1,16 +1,28 @@
+const path = require('path');
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
+
+
+// // path: ./my-project/config/database.js
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "storyesdb.c5ysy25j6dpi.us-east-1.rds.amazonaws.com"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "storyesdb"),
+//       user: env("DATABASE_USERNAME", "username"), 
+//       password: env("DATABASE_PASSWORD", "adminroot"),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
